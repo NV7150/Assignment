1f410a3a7498d8d5e9b79592aa94d4aa9d692e2d
NV7150 <7150macNV777DAI@gmail.com>
2021-06-17 17:19:01 +0900

    connect to db

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
 from fastapi import FastAPI
+from db import session
+from models import UserTable, User
 
 app = FastAPI()
 
@@ -9,3 +11,16 @@ def root_response():
         "langs": "C/C++, Java, C#, JS",
         "keywords": "IoT, VR, Application"
     }
+
+@app.get("/users/{user_id}")
+def get_user(user_id: int):
+    user = session.query(UserTable).filter(UserTable.id == user_id).first()
+    return user
+
+@app.post("/users")
+async def create_user(name: str, keyword: str):
+    user = UserTable()
+    user.name = name
+    user.keyword = keyword
+    session.add(user)
+    session.commit()
--- readme.md
+++ readme.md
@@ -1,2 +1,5 @@
 # How to use
-Run `docker-compose up` in HttpResponser Directory. 
+Run `docker-compose up -d --build` in HttpResponser Directory. <br>
+This is so unstable program, so sometimes it responses Internal Server Error, 
+but please don't mind and try some times more. <br>
+Please access `localhost:8080/docs` to check what can this program do.
